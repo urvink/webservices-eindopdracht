f38d0173e7baf71f70774bfd65c6e1214fbc40bb
urvink <urvinkirindongo@gmail.com>
2015-04-28 04:43:01 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,19 +8,10 @@
 
 $host = 'localhost';
 $db = '0756219';
-
-
-if($_SERVER['HTTP_HOST'] == 'localhost'){
-	$username = 'root';
-	$password = '';
-}
-else{
-	$username = '0756219';
-	$password = 'mandidios';
-}
+$username = 'root';
+$password = '';
 
 $link = mysqli_connect($host,$username,$password,$db);
-//print_r($username.' '.$password);
 /* check connection */
 if (mysqli_connect_errno()) {
 	printf("Connect failed: %s\n", mysqli_connect_error());
